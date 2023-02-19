97c67ca2399034a11ad2675d56b55d19661438a2
Souhail Aniba <aniba.souhail@ensam-casa.ma>
2023-02-19 14:37:39 +0100

    idafat 7

---
+++ dashboard/src/app/dashboard.service.ts
@@ -0,0 +1,31 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DashboardService {
+
+  constructor(private http: HttpClient) { }
+
+  url:string = "http://localhost:8000" ;
+
+  getTotalUsers(){
+    return this.http.get<any>(this.url+'/api/totalUsers');
+  }
+
+  getTotalProducts(){
+    return this.http.get<any>(this.url+'/api/totalProducts');
+  }
+
+  getTotalRevenue(){
+    return this.http.get<any>(this.url+'/api/totalRevenue');
+  }
+
+  /*
+  getTotalOrders(){
+    return this.http.get<any>(this.url+'/api/totalOrders');
+  }
+  */
+
+}
--- dashboard/src/app/pages/dashboard/dashboard.component.ts
+++ dashboard/src/app/pages/dashboard/dashboard.component.ts
@@ -1,13 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, OnInit  } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { DashboardService } from 'src/app/dashboard.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent {
-  totalUsers = 1000;
-  totalOrders = 500;
-  totalRevenue = 20000;
-  totalItems = 8000;
+
+export class DashboardComponent implements OnInit {
+  
+  totalUsers = 0;
+  totalItems = 0;
+  totalOrders = 0;
+  totalRevenue = 0;
+
+  constructor(private service: DashboardService) {}
+
+  ngOnInit():void{
+    this.getTotalUsers()
+    this.getTotalProducts()
+    this.getTotalRevenue()
+    // this.getTotalOrders()
+  }
+
+  getTotalUsers(){
+    this.service.getTotalUsers().subscribe((res:any)=>{
+      this.totalUsers= res
+      console.log(res)
+    }, error =>{
+     alert(error)
+    })
+  }
+
+  getTotalProducts(){
+    this.service.getTotalProducts().subscribe((res:any)=>{
+      this.totalItems= res
+      console.log(res)
+    }, error =>{
+     alert(error)
+    })
+  }
+
+  getTotalRevenue(){
+    this.service.getTotalRevenue().subscribe((res:any)=>{
+      this.totalRevenue= res
+      console.log(res)
+    }, error =>{
+     alert(error)
+    })
+  }
+
+  /*
+  getTotalRevenue(){
+    this.service.getTotalRevenue().subscribe((res:any)=>{
+      this.totalRevenue= res
+      console.log(res)
+    }, error =>{
+     alert(error)
+    })
+  }
+  */
+
 }
